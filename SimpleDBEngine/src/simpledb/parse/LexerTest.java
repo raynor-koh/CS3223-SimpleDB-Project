5526b31214c173fe5271512bab2c9af640e54df6
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-23 01:21:09 +0800

    test: update LexerTest for new comparison operators

--- SimpleDBEngine/src/simpledb/parse/LexerTest.java
+++ SimpleDBEngine/src/simpledb/parse/LexerTest.java
@@ -1,28 +1,38 @@
 package simpledb.parse;
+
 import java.util.Scanner;
 
 // Will successfully read in lines of text denoting an
-// SQL expression of the form "id = c" or "c = id".
+// SQL expression of the form "id opr  c" or "c opr id".
 
 public class LexerTest {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			String s = sc.nextLine();
-			Lexer lex = new Lexer(s);
-			String x; int y;
-			if (lex.matchId()) {
-				x = lex.eatId();
-				lex.eatDelim('=');
-				y = lex.eatIntConstant();
-			}
-			else {
-				y = lex.eatIntConstant();
-				lex.eatDelim('=');
-				x = lex.eatId();	
-			}
-			System.out.println(x + " equals " + y);
-		}
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            Lexer lex = new Lexer(s);
+            String x;
+            String opr;
+            int y;
+            try {
+                if (lex.matchId()) {
+                    x = lex.eatId();
+                    opr = lex.eatOpr();
+                    y = lex.eatIntConstant();
+
+                    System.out.println(x + " " + opr + " " + y);
+                } else {
+                    y = lex.eatIntConstant();
+                    opr = lex.eatOpr();
+                    x = lex.eatId();
+
+                    System.out.println(y + " " + opr + " " + x);
+                }
+            } catch (BadSyntaxException exception) {
+                System.out.println("Invalid comparison operator");
+            }
+
+        }
+        sc.close();
+    }
 }
