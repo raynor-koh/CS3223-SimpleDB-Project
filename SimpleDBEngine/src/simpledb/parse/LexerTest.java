13a13e67170a4b51a6b9e564941f7a897f746279
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-23 04:14:09 +0800

    chore: remove unrelated formatting changes

--- SimpleDBEngine/src/simpledb/parse/LexerTest.java
+++ SimpleDBEngine/src/simpledb/parse/LexerTest.java
@@ -1,38 +1,37 @@
 package simpledb.parse;
-
 import java.util.Scanner;
 
 // Will successfully read in lines of text denoting an
 // SQL expression of the form "id opr  c" or "c opr id".
 
 public class LexerTest {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String s = sc.nextLine();
-            Lexer lex = new Lexer(s);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String s = sc.nextLine();
+			Lexer lex = new Lexer(s);
             String x;
             String opr;
             int y;
             try {
-                if (lex.matchId()) {
-                    x = lex.eatId();
-                    opr = lex.eatOpr();
-                    y = lex.eatIntConstant();
+			if (lex.matchId()) {
+				x = lex.eatId();
+                opr = lex.eatOpr();
+				y = lex.eatIntConstant();
 
                     System.out.println(x + " " + opr + " " + y);
                 } else {
-                    y = lex.eatIntConstant();
+				y = lex.eatIntConstant();
                     opr = lex.eatOpr();
-                    x = lex.eatId();
+				x = lex.eatId();	
 
                     System.out.println(y + " " + opr + " " + x);
-                }
+			}
             } catch (BadSyntaxException exception) {
                 System.out.println("Invalid comparison operator");
             }
 
-        }
-        sc.close();
-    }
+		}
+		sc.close();
+	}
 }
--- SimpleDBEngine/src/simpledb/query/Term.java
+++ SimpleDBEngine/src/simpledb/query/Term.java
@@ -5,23 +5,22 @@ import simpledb.record.*;
 
 /**
  * A term is a comparison between two expressions.
- * 
  * @author Edward Sciore
  *
  */
 public class Term {
-    private Expression lhs, rhs;
-    private String operator;
-
-    /**
-     * Create a new term that compares two expressions
-     * for equality.
+   private Expression lhs, rhs;
+   private String operator;
+   
+   /**
+    * Create a new term that compares two expressions
+    * for equality.
      * This is kept for backward compatiability.
      * 
      * @param lhs the LHS expression
      * @param rhs the RHS expression
-     */
-    public Term(Expression lhs, Expression rhs) {
+    */
+   public Term(Expression lhs, Expression rhs) {
         this(lhs, "=", rhs);
     }
 
@@ -34,22 +33,21 @@ public class Term {
      * 
      */
     public Term(Expression lhs, String operator, Expression rhs) {
-        this.lhs = lhs;
-        this.operator = operator;
-        this.rhs = rhs;
-    }
-
-    /**
-     * Return true if both of the term's expressions
-     * evaluate to the same constant,
-     * with respect to the specified scan.
-     * 
-     * @param s the scan
-     * @return true if both expressions have the same value in the scan
-     */
-    public boolean isSatisfied(Scan s) {
-        Constant lhsval = lhs.evaluate(s);
-        Constant rhsval = rhs.evaluate(s);
+      this.lhs = lhs;
+      this.operator = operator;
+      this.rhs = rhs;
+   }
+   
+   /**
+    * Return true if both of the term's expressions
+    * evaluate to the same constant,
+    * with respect to the specified scan.
+    * @param s the scan
+    * @return true if both expressions have the same value in the scan
+    */
+   public boolean isSatisfied(Scan s) {
+      Constant lhsval = lhs.evaluate(s);
+      Constant rhsval = rhs.evaluate(s);
 
         return compare(lhsval, rhsval);
     }
@@ -90,18 +88,17 @@ public class Term {
                 throw new IllegalArgumentException(
                         "Unsupported comparison operator: " + operator);
         }
-    }
-
-    /**
-     * Calculate the extent to which selecting on the term reduces
-     * the number of records output by a query.
-     * For example if the reduction factor is 2, then the
-     * term cuts the size of the output in half.
-     * 
-     * @param p the query's plan
-     * @return the integer reduction factor.
-     */
-    public int reductionFactor(Plan p) {
+   }
+   
+   /**
+    * Calculate the extent to which selecting on the term reduces 
+    * the number of records output by a query.
+    * For example if the reduction factor is 2, then the
+    * term cuts the size of the output in half.
+    * @param p the query's plan
+    * @return the integer reduction factor.
+    */
+   public int reductionFactor(Plan p) {
         if (!isEqualityComparison() && (lhs.isFieldName() || rhs.isFieldName())) {
             /*
              * SimpleDB does not maintain value ranges or distribution statistics,
@@ -113,27 +110,27 @@ public class Term {
             return 2;
         }
 
-        String lhsName, rhsName;
-        if (lhs.isFieldName() && rhs.isFieldName()) {
-            lhsName = lhs.asFieldName();
-            rhsName = rhs.asFieldName();
-            return Math.max(p.distinctValues(lhsName),
-                    p.distinctValues(rhsName));
-        }
-        if (lhs.isFieldName()) {
-            lhsName = lhs.asFieldName();
-            return p.distinctValues(lhsName);
-        }
-        if (rhs.isFieldName()) {
-            rhsName = rhs.asFieldName();
-            return p.distinctValues(rhsName);
-        }
-        // otherwise, the term equates constants
+      String lhsName, rhsName;
+      if (lhs.isFieldName() && rhs.isFieldName()) {
+         lhsName = lhs.asFieldName();
+         rhsName = rhs.asFieldName();
+         return Math.max(p.distinctValues(lhsName),
+                         p.distinctValues(rhsName));
+      }
+      if (lhs.isFieldName()) {
+         lhsName = lhs.asFieldName();
+         return p.distinctValues(lhsName);
+      }
+      if (rhs.isFieldName()) {
+         rhsName = rhs.asFieldName();
+         return p.distinctValues(rhsName);
+      }
+      // otherwise, the term equates constants
         if (compare(lhs.asConstant(), rhs.asConstant()))
-            return 1;
-        else
-            return Integer.MAX_VALUE;
-    }
+         return 1;
+      else
+         return Integer.MAX_VALUE;
+   }
 
     /**
      * The optimizer uses equatesWithConstant() and equatesWithField() to
@@ -146,71 +143,68 @@ public class Term {
     private boolean isEqualityComparison() {
         return "=".equals(operator);
     }
-
-    /**
-     * Determine if this term is of the form "F=c"
-     * where F is the specified field and c is some constant.
-     * If so, the method returns that constant.
-     * If not, the method returns null.
-     * 
-     * @param fldname the name of the field
-     * @return either the constant or null
-     */
-    public Constant equatesWithConstant(String fldname) {
+   
+   /**
+    * Determine if this term is of the form "F=c"
+    * where F is the specified field and c is some constant.
+    * If so, the method returns that constant.
+    * If not, the method returns null.
+    * @param fldname the name of the field
+    * @return either the constant or null
+    */
+   public Constant equatesWithConstant(String fldname) {
         if (!isEqualityComparison()) {
             return null;
         }
 
-        if (lhs.isFieldName() &&
-                lhs.asFieldName().equals(fldname) &&
-                !rhs.isFieldName())
-            return rhs.asConstant();
-        else if (rhs.isFieldName() &&
-                rhs.asFieldName().equals(fldname) &&
-                !lhs.isFieldName())
-            return lhs.asConstant();
-        else
-            return null;
-    }
-
-    /**
-     * Determine if this term is of the form "F1=F2"
-     * where F1 is the specified field and F2 is another field.
-     * If so, the method returns the name of that field.
-     * If not, the method returns null.
-     * 
-     * @param fldname the name of the field
-     * @return either the name of the other field, or null
-     */
-    public String equatesWithField(String fldname) {
+      if (lhs.isFieldName() &&
+          lhs.asFieldName().equals(fldname) &&
+          !rhs.isFieldName())
+         return rhs.asConstant();
+      else if (rhs.isFieldName() &&
+               rhs.asFieldName().equals(fldname) &&
+               !lhs.isFieldName())
+         return lhs.asConstant();
+      else
+         return null;
+   }
+   
+   /**
+    * Determine if this term is of the form "F1=F2"
+    * where F1 is the specified field and F2 is another field.
+    * If so, the method returns the name of that field.
+    * If not, the method returns null.
+    * @param fldname the name of the field
+    * @return either the name of the other field, or null
+    */
+   public String equatesWithField(String fldname) {
         if (!isEqualityComparison()) {
             return null;
         }
 
-        if (lhs.isFieldName() &&
-                lhs.asFieldName().equals(fldname) &&
-                rhs.isFieldName())
-            return rhs.asFieldName();
-        else if (rhs.isFieldName() &&
-                rhs.asFieldName().equals(fldname) &&
-                lhs.isFieldName())
-            return lhs.asFieldName();
-        else
-            return null;
-    }
-
-    /**
-     * Return true if both of the term's expressions
-     * apply to the specified schema.
-     * 
-     * @param sch the schema
-     * @return true if both expressions apply to the schema
-     */
-    public boolean appliesTo(Schema sch) {
-        return lhs.appliesTo(sch) && rhs.appliesTo(sch);
-    }
-
-    public String toString() {
+      if (lhs.isFieldName() &&
+          lhs.asFieldName().equals(fldname) &&
+          rhs.isFieldName())
+         return rhs.asFieldName();
+      else if (rhs.isFieldName() &&
+               rhs.asFieldName().equals(fldname) &&
+               lhs.isFieldName())
+         return lhs.asFieldName();
+      else
+         return null;
+   }
+   
+   /**
+    * Return true if both of the term's expressions
+    * apply to the specified schema.
+    * @param sch the schema
+    * @return true if both expressions apply to the schema
+    */
+   public boolean appliesTo(Schema sch) {
+      return lhs.appliesTo(sch) && rhs.appliesTo(sch);
+   }
+   
+   public String toString() {
         return lhs.toString() + operator + rhs.toString();
-    }
+   }
 }
