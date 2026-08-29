c6e27fb44b0319d5ed8bb633b570c168d2878bf9
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-30 01:05:01 +0800

    test: add index to pass IndexRetrievalTest

--- SimpleDBEngine/src/simpledb/test/CreateStudentDB.java
+++ SimpleDBEngine/src/simpledb/test/CreateStudentDB.java
@@ -16,6 +16,10 @@ public class CreateStudentDB {
          planner.executeUpdate(s, tx);
          System.out.println("Table STUDENT created.");
 
+         s = "create index idx_stud_major on STUDENT(MajorId)";
+         planner.executeUpdate(s, tx);
+         System.out.println("Index idx_stud_major created.");
+
          s = "insert into STUDENT(SId, SName, MajorId, GradYear) values ";
          String[] studvals = { "(1, 'joe', 10, 2021)",
                "(2, 'amy', 20, 2020)",
@@ -74,6 +78,10 @@ public class CreateStudentDB {
          s = "create table ENROLL(EId int, StudentId int, SectionId int, Grade varchar(2))";
          planner.executeUpdate(s, tx);
          System.out.println("Table ENROLL created.");
+
+         s = "create index idx_enroll_sid on ENROLL(StudentId)";
+         planner.executeUpdate(s, tx);
+         System.out.println("Index idx_enroll_sid created.");
 
          s = "insert into ENROLL(EId, StudentId, SectionId, Grade) values ";
          String[] enrollvals = { "(14, 1, 13, 'A')",
